e790ad3d4e7b0497eb76ffa606c96b8b418125aa
oskfelin <oskfelin@gmail.com>
2017-06-29 16:49:02 +0200

    poprawiona mechanika i wygląd gry

---
+++ scripts/game-start.js
@@ -0,0 +1,13 @@
+/**
+ * Created by oskfelin on 29.06.17.
+ */
+var gameBoard = $('formularz');
+var $formField = $('#form-field');
+var $formGame = $('#form');
+$formGame.on('submit', function (event) {
+    $formField.hide();
+    gameBoard.append()
+});
+
+
+
