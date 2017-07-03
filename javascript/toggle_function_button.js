32c16e8ab9a6f7ae6ec9a45b06001772c607cc7a
marcin-kopystynski <marcin.kopystynski@gmail.com>
2017-07-03 10:26:33 +0200

    poprawiona nazwa przycisku do rozwijania

--- javascript/toggle_function_button.js
+++ javascript/toggle_function_button.js
@@ -2,12 +2,12 @@ $(function(){
     $("#slide-toogle").click(function(){
 
         if ($('#extended-functions').css('display') == 'none') {
-            $("#slide-toogle").text('Naciśnij żeby zwinąć');
+            $("#slide-toogle").text('Naciśnij, aby zwinąć');
             $("#extended-functions").slideDown();
         }
         else {
             $("#extended-functions").slideUp();
-            $("#slide-toogle").text('Naciśnij żeby rozwinąć');
+            $("#slide-toogle").text('Naciśnij, aby poznać pozostałe funkcje');
         }
     });
 });
